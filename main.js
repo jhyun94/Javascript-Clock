bb39d923bd34f17e32f0f474f1b2331dbefb6a78
jhyun94 <hyunjonathan@gmail.com>
2017-03-16 15:44:21 -0500

    displays clock and hand

---
+++ main.js
@@ -0,0 +1,7 @@
+var date = new Date ();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
