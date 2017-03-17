26bf96b06b6e62ea680406393b36503faf37673b
jhyun94 <hyunjonathan@gmail.com>
2017-03-17 14:11:58 -0500

    trasnalted seconds to degree to get the current angle

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
 
 setInterval(setTime, 1000);
-
-
-
-
-
 
 
 function setTime(){
@@ -13,4 +10,13 @@ function setTime(){
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var seconds = date.getSeconds();
+
+  var secondsDegree = (seconds / 60 ) * 360 + 90;
+  second.style.transform = `rotate(${secondsDegree}deg)`;
+
+  var minDegree = ((minutes / 60 ) * 360) + 90;
+  minute.style.transform = `rotate(${minDegree}deg)`;
+
+  var hourDegree = ((hours / 12) * 360) + 90;
+  hour.style.transform = `rotate(${hourDegree}deg)`;
 }
