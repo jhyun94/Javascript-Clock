4df650006bd6898213b8a2a0f6c70d1e885eed91
jhyun94 <hyunjonathan@gmail.com>
2017-03-17 13:23:40 -0500

    fixed hand position and transition

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-var date = new Date ();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
-console.log(hours);
-console.log(minutes);
-console.log(seconds);
+
+
+setInterval(setTime, 1000);
+
+
+
+
+
+
+
+function setTime(){
+  var date = new Date ();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+}
